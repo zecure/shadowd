d8dc10fe8af3ad0e4b372eb7ebe37d1fb76eb4bc
zit-hb <hb@zecure.org>
2015-11-08 16:20:19 +0100

    Readded mode check.

--- src/connection.cpp
+++ src/connection.cpp
@@ -268,11 +268,15 @@ void swd::connection::handle_read(const boost::system::error_code& e,
 			throw swd::exceptions::connection_exception(STATUS_BAD_REQUEST);
 		}
 
-		if (request_->is_threat()) {
-			reply_->set_status(STATUS_BAD_REQUEST);
-		} else if (request_->has_threats()) {
-			reply_->set_threats(request_handler.get_threats());
-			reply_->set_status(STATUS_ATTACK);
+		if (request_->get_profile()->get_mode() == MODE_ACTIVE) {
+			if (request_->is_threat()) {
+				reply_->set_status(STATUS_BAD_REQUEST);
+			} else if (request_->has_threats()) {
+				reply_->set_threats(request_handler.get_threats());
+				reply_->set_status(STATUS_ATTACK);
+			} else {
+				reply_->set_status(STATUS_OK);
+			}
 		} else {
 			reply_->set_status(STATUS_OK);
 		}
